3f122a13366dc5869524794a282a5d566c34fa63
master <2461028063@qq.com>
2022-04-12 23:35:12 +0800

    master‘

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @Author: devilwzx
+ * @Date: 2022-03-24 21:16:50
+ * @LastEditors: devilwzx
+ * @LastEditTime: 2022-04-02 13:07:28
+ * @FilePath: \HTML_Code\index.php
+ * @Description: 
+ * 
+ * @My website: devilwzx.top
+ * Copyright (c) 2022 by devilwzx, All Rights Reserved. 
+ */
+echo "woaini"
+?>
